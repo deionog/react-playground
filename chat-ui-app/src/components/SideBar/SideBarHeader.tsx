1557e9022b582f5b661ce7083e7d4af1d82b28bb
Deion OGarro <deionog@gmail.com>
2026-02-08 09:28:34 -0500

    feat: update SideBarHeader component and styles for new chat button

--- chat-ui-app/src/components/SideBar/SideBarHeader.tsx
+++ chat-ui-app/src/components/SideBar/SideBarHeader.tsx
@@ -1,13 +1,22 @@
 import sidebarBtn from '../../assets/sidebar_17261359.png'
+import './sidebar.css';
+
 export function SideBarHeader() {
   return (
     <div className="p-3">
         <div className="sidebar-toggle">
             <button ><img src={sidebarBtn} alt="Sidebar Button" className="size-6 shrink-0" /></button>
         </div>
-      <button className="w-full rounded-md border border-zinc-700 px-3 py-2 text-sm text-left hover:bg-zinc-800 transition">
-        + New chat
-      </button>
+        <div className='w-full flex justify-center items-center'>
+          <button data-label="+ New Chat" className="btn w-full
+          cursor-pointer
+          relative
+          rounded-md px-3 py-2 text-sm text-center
+          bg-linear-to-r/decreasing from-violet-700 via-lime-300 to-violet-700 z-1
+          ">
+          </button>
+        </div>
+
     </div>
   )
 }
